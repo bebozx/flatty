5dcb875824dcc46416df8b37c95e170bb7c6d04d
bebozx <masterbob2030@gmail.com>
2026-01-28 01:11:41 +0200

    Modify signing config to include alias check
    Add debug print statement for checking key alias.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,10 @@ android {
     // التعديل الصحيح للغة Kotlin (KTS)
     signingConfigs {
         create("release") {
-            // هنا بنسحب القيم اللي باعتنها من ملف الـ YAML
-            keyAlias = System.getenv("ALIAS") ?: ""
+            // سطر التجسس (اختياري للتأكد فقط)
+            println("Checking Alias: ${System.getenv("ALIAS")}")
+            
+            keyAlias = System.getenv("ALIAS") ?: "nazer" // لو ملقاش المتغير هيجرب nazer
             keyPassword = System.getenv("KEY_PASSWORD") ?: ""
             storeFile = file("key.jks")
             storePassword = System.getenv("KEY_STORE_PASSWORD") ?: ""
