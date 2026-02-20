94654899b6e8c33d699bb25ad755b9bf4e50745d
bebozx <masterbob2030@gmail.com>
2026-02-21 00:16:00 +0200

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -21,7 +21,7 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     // نستخدم إصدار 8.2.1 لضمان التوافق مع GitHub Actions
     id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
 }
 
 include(":app")
